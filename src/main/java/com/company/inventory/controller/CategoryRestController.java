a7953a9afd4bcc44cb83426679ab94e855d4be06
alvdela <alvarodlr002@gmail.com>
2024-08-25 12:40:56 +0200

    Servicio de categorías terminado.

--- src/main/java/com/company/inventory/controller/CategoryRestController.java
+++ src/main/java/com/company/inventory/controller/CategoryRestController.java
@@ -2,24 +2,59 @@ package com.company.inventory.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.company.inventory.model.Category;
 import com.company.inventory.response.CategoryResponseRest;
-import com.company.inventory.services.ICategoryService;
+import com.company.inventory.services.ICategoryServiceImpl;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/api/v1")
 public class CategoryRestController {
 	
 	@Autowired
-	private ICategoryService categoryService;
+	private ICategoryServiceImpl categoryService;
 
 	@GetMapping("/categories")
 	public ResponseEntity<CategoryResponseRest> searchCategories(){
 		
 		ResponseEntity<CategoryResponseRest> response = categoryService.search();
+		return response;
+	}
+	
+	@GetMapping("/categories/{id}")
+	public ResponseEntity<CategoryResponseRest> searchCategoryById(@PathVariable Long id){
+		
+		ResponseEntity<CategoryResponseRest> response = categoryService.searchById(id);
+		return response;
+	}
+	
+	@PostMapping("/categories")
+	public ResponseEntity<CategoryResponseRest> save(@RequestBody Category category){
+		
+		ResponseEntity<CategoryResponseRest> response = categoryService.save(category);
+		return response;
+	}
+	
+	@PutMapping("/categories/{id}")
+	public ResponseEntity<CategoryResponseRest> update(@PathVariable Long id, @RequestBody Category category){
+		
+		ResponseEntity<CategoryResponseRest> response = categoryService.update(category, id);
+		return response;
+	}
+	
+	@DeleteMapping("/categories/{id}")
+	public ResponseEntity<CategoryResponseRest> deleteById(@PathVariable Long id){
+		
+		ResponseEntity<CategoryResponseRest> response = categoryService.deleteById(id);
 		return response;
 	}
 }
---
+++ src/main/java/com/company/inventory/services/ICategoryServiceImpl.java
@@ -0,0 +1,20 @@
+package com.company.inventory.services;
+
+import org.springframework.http.ResponseEntity;
+
+import com.company.inventory.model.Category;
+import com.company.inventory.response.CategoryResponseRest;
+
+public interface ICategoryServiceImpl {
+
+	public ResponseEntity<CategoryResponseRest> search();
+	
+	public ResponseEntity<CategoryResponseRest> searchById(Long id);
+	
+	public ResponseEntity<CategoryResponseRest> save(Category category);
+	
+	public ResponseEntity<CategoryResponseRest> update(Category category, Long id);
+
+	public ResponseEntity<CategoryResponseRest> deleteById(Long id);
+
+}
